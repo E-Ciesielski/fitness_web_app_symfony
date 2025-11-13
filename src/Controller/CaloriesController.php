c89445ebf649a1197049e698609389b4af8c01d7
Eryk <eryk-ciesielski@outlook.com>
2025-11-13 15:58:11 +0100

    implement date picking for calories page

--- src/Controller/CaloriesController.php
+++ src/Controller/CaloriesController.php
@@ -4,14 +4,30 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/calories')]
 class CaloriesController extends AbstractController
 {
     #[Route('', name: 'app_calories_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(#[MapQueryParameter] ?string $date = null): Response
     {
-        return $this->render('calories/index.html.twig', []);
+        if($date === null)
+        {
+            $date = new \DateTimeImmutable("now");
+        }
+        else
+        {
+            $date = \DateTimeImmutable::createFromFormat('Y-m-d', $date);
+            if($date === false)
+            {
+                throw $this->createNotFoundException("Date is not valid");
+            }
+        }
+
+        return $this->render('calories/index.html.twig', [
+            'date' => $date->format('Y-m-d'),
+        ]);
     }
 }
